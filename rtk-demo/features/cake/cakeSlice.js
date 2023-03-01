97440d1c469fa474a992ca5a553248a898301754
yusufhikmat <hikmatyusuff2020@gmail.com>
2023-03-01 04:01:34 -1200

    added redux toolkits folder

---
+++ rtk-demo/features/cake/cakeSlice.js
@@ -0,0 +1,20 @@
+const createSlice = require('@reduxjs/toolkit').createSlice
+
+
+const initialState = {
+    numOfCakes : 10,
+}
+const cakeSlice = createSlice({
+    name:"cake",
+    initialState:initialState,
+    reducer:{
+        ordered:(state)=>{
+            state.numOfCakes--
+        },
+        restoked:(state, action)=>{
+            state.numOfCakes += action.payload
+        }
+    }
+})
+module.exports = cakeSlice.reducer
+module.exports.cakeActions = cakeSlice.actions
